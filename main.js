1ca60144e4730b9952c0d080b1b172d43c0b26a7
Sten Orasmae <sten9911@gmail.com>
2022-04-13 15:19:23 +0300

    basic info to domm

--- main.js
+++ main.js
@@ -5,7 +5,19 @@ let API = new FetchWrapper("https://intra.proekspert.ee/pulse-johvi")
 API.post("/auth", {"username":"user", "password":"user"}).then(data=>{
 API.token = data.token
 console.log(API.token)
-API.get("api/employees", data.token).then(data => {
+
+let employeeData
+API.get("/api/employees/9", data.token).then(data => {
     console.log(data)
+    employeeData = data
+    SetEmployeeData()
 })
+
+function SetEmployeeData() {
+    document.getElementById("person_name").innerHTML = employeeData.name
+    document.getElementById("title").innerHTML = employeeData.title
+    document.getElementById("about_me").innerHTML = employeeData.about    
+    document.getElementById("skills").innerHTML = employeeData.skills    
+}
 })
+
