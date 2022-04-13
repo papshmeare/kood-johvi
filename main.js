ffd811e2a3ac80a0a591db86268b0e78e07c6032
papshmeare <aleksei.baranovski.tln@gmail.com>
2022-04-13 14:15:09 +0300

    Fetch wrapper, get token

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+import {FetchWrapper} from "./fetchWrapper.js"
+
+let API = new FetchWrapper("https://intra.proekspert.ee/pulse-johvi")
+
+API.post("/auth", {"username":"user", "password":"user"}).then(data=>{
+console.log(data)
+})
+
 console.log("hello")
